bf465499c3357246dcb03fe321a1594b911f1529
Sandeep Singh <singh.deep8302@gmail.com>
2024-01-07 13:00:56 -0500

    added my socials

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,9 +1,47 @@
 import React from "react";
 
 export function SideNav() {
+  const Github = `${process.env.PUBLIC_URL}/images/Github.png`;
+  const Instagram = `${process.env.PUBLIC_URL}/images/Instagram.png`;
+  const Linkedin = `${process.env.PUBLIC_URL}/images/Linkedin.png`;
+
+  const handleOpenWebsite = (social) => {
+    let websiteURL = "";
+    if (social === "Github") {
+      websiteURL = "https://github.com/SandeepS3";
+    } else if (social === "Instagram") {
+      websiteURL = "https://www.instagram.com/sandeeps3_/";
+    } else if (social === "Linkedin") {
+      websiteURL = "https://www.linkedin.com/in/sandeep-singh-00a903200/";
+    } else {
+      return;
+    }
+    window.open(websiteURL, "_blank");
+  };
+
   return (
     <div style={styles.sideNavStyle}>
-      <h style={{ color: "white" }}>Sandeep Singh</h>
+      <h style={styles.name}>Sandeep Singh</h>
+      <div style={styles.socials}>
+        <img
+          style={{ position: "relative", left: "50px", cursor: "pointer" }}
+          src={Github}
+          alt="Github"
+          onClick={() => handleOpenWebsite("Github")}
+        />
+        <img
+          style={{ position: "relative", left: "80px", cursor: "pointer" }}
+          src={Instagram}
+          alt="Instagram"
+          onClick={() => handleOpenWebsite("Instagram")}
+        />
+        <img
+          style={{ position: "relative", left: "130px", cursor: "pointer" }}
+          src={Linkedin}
+          alt="Linkedin"
+          onClick={() => handleOpenWebsite("Linkedin")}
+        />
+      </div>
       <a
         href="/"
         style={styles.linkStyle}
@@ -18,14 +56,12 @@ export function SideNav() {
 
 const styles = {
   sideNavStyle: {
+    display: "flex",
+    flexDirection: "column",
     alignItems: "center",
-    // height: "100%",
     width: "25vw",
-    // position: "fixed",
-    zIndex: 1,
     backgroundColor: "#646464",
-    paddingTop: "1.2em",
-    // border: "1px solid rgba(255, 255, 255, 0.10)",
+    paddingTop: "5.5em",
   },
 
   linkStyle: {
@@ -39,7 +75,15 @@ const styles = {
     color: "white",
     display: "block",
   },
-
+  name: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: "2.8rem",
+  },
+  socials: {
+    flexDirection: "row",
+    alignSelf: "flex-start",
+  },
   hoverStyle: {
     backgroundColor: "rgba(255, 255, 255, 0.15)",
   },
