a4f201b2b98117dd4f63753b5aea806154bf76be
Sandeep Singh <singh.deep8302@gmail.com>
2024-01-08 02:03:42 -0500

    about page for mobile

---
+++ src/pages/AboutStyles.js
@@ -0,0 +1,73 @@
+const styles = {
+  container: {
+    display: "flex",
+    backgroundColor: "#4A4A4A",
+    flexDirection: "column",
+    borderRadius: "1.25em",
+    height: "100%",
+  },
+  contactInfo: {
+    display: "flex",
+    flexDirection: "row",
+    padding: "5vh 6vw",
+    justifyContent: "space-between",
+  },
+  contactText: {
+    color: "white",
+    fontSize: "3vh",
+    fontFamily: "Korona One",
+    textShadow: "0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.7)",
+  },
+  aboutMe: {
+    display: "flex",
+  },
+  leftHalf: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+  },
+  rightHalf: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+  },
+  aboutText: {
+    color: "black",
+    fontFamily: "Regular",
+    fontSize: "4vh",
+    padding: "3vh",
+    margin: "2vw",
+    textShadow: "0.125rem 0.125rem 0.25rem rgba(255, 255, 255, 0.8)",
+    backgroundColor: "#646464",
+    borderRadius: "1.25em",
+  },
+  imageBox: {
+    padding: "1vh",
+    backgroundColor: "#646464",
+    borderRadius: "1.25em",
+  },
+  image: {
+    width: "25vw",
+    height: "75vh",
+    borderRadius: "1.25em",
+  },
+};
+
+const mediaStyles = {
+  contactInfo: {
+    display: "flex",
+    flexDirection: "column",
+    padding: "5vh 6vw",
+    // justifyContent: "space-between",
+  },
+  aboutMe: {
+    display: "flex",
+    flexDirection: "column",
+  },
+};
+
+const mergedStyles = window.matchMedia("(max-width: 600px)").matches
+  ? { ...styles, ...mediaStyles }
+  : styles;
+
+export { mergedStyles as styles };
