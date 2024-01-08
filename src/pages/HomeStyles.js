e09dc82f83854fe4a9a4ded685ecb52569474452
Sandeep Singh <singh.deep8302@gmail.com>
2024-01-07 20:08:07 -0500

    small mroe changes

--- src/pages/HomeStyles.js
+++ src/pages/HomeStyles.js
@@ -29,15 +29,31 @@ const styles = {
 };
 
 const mediaStyles = {
+  background: {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/Beach.png)`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    WebkitBackgroundSize: "contain",
+    backgroundSize: "100% 275%",
+    minHeight: "100vh",
+    borderRadius: "1.25em",
+  },
+  info: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingLeft: "5vw",
+    paddingTop: "65vh",
+  },
   name: {
     fontFamily: "Acme",
-    fontSize: "3.25rem",
+    fontSize: "2.25rem",
     color: "white",
-    marginBottom: "-1.75rem",
+    marginBottom: "1rem",
   },
   title: {
     fontFamily: "Actor",
-    fontSize: "0.563rem",
+    fontSize: "1.5rem",
     color: "white",
   },
 };
