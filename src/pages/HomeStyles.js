2d51610290414c43a9e4b5f39341827c20a8b82d
Sandeep Singh <singh.deep8302@gmail.com>
2024-01-07 20:02:45 -0500

    edits to make homepage mobile friendly

---
+++ src/pages/HomeStyles.js
@@ -0,0 +1,49 @@
+const styles = {
+  background: {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/Beach.png)`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    WebkitBackgroundSize: "cover",
+    backgroundSize: "100% 275%",
+    minHeight: "100vh",
+    borderRadius: "1.25em",
+  },
+  info: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    paddingLeft: "5vw",
+    paddingTop: "65vh",
+  },
+  name: {
+    fontFamily: "Acme",
+    fontSize: "5.25rem",
+    color: "white",
+    marginBottom: "-1.75rem",
+  },
+  title: {
+    fontFamily: "Actor",
+    fontSize: "1.563rem",
+    color: "white",
+  },
+};
+
+const mediaStyles = {
+  name: {
+    fontFamily: "Acme",
+    fontSize: "3.25rem",
+    color: "white",
+    marginBottom: "-1.75rem",
+  },
+  title: {
+    fontFamily: "Actor",
+    fontSize: "0.563rem",
+    color: "white",
+  },
+};
+
+const mergedStyles = window.matchMedia("(max-width: 600px)").matches
+  ? { ...styles, ...mediaStyles }
+  : styles;
+
+export { mergedStyles as styles };
