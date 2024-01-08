3b8fc5751f1e7235b73b2b1b86637010de6ee4b7
Sandeep Singh <singh.deep8302@gmail.com>
2024-01-07 20:49:54 -0500

    removed boxcontainer css

--- src/components/BoxContainer.js
+++ src/components/BoxContainer.js
@@ -1,13 +1,25 @@
 import React from "react";
-import "./BoxContainer.css";
 import { Outlet } from "react-router";
 
 export function BoxContainer() {
   return (
-    <div className="box-container">
-      <div className="page-container">
+    <div style={styles.boxContainer}>
+      <div style={styles.pageContainer}>
         <Outlet />
       </div>
     </div>
   );
 }
+
+const styles = {
+  boxContainer: {
+    display: "flex",
+    flex: "1",
+    backgroundColor: "#4A4A4A",
+  },
+  pageContainer: {
+    height: "100vh",
+    margin: "1vh",
+    width: "100%",
+  },
+};
