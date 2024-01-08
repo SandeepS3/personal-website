7421c962aef4f3ae3346f65dfb22ee49c687b7dd
Sandeep Singh <singh.deep8302@gmail.com>
2024-01-08 02:09:03 -0500

    font size changes

--- src/pages/AboutStyles.js
+++ src/pages/AboutStyles.js
@@ -54,11 +54,27 @@ const styles = {
 };
 
 const mediaStyles = {
+  contactText: {
+    color: "white",
+    fontSize: "2.5vh",
+    fontFamily: "Korona One",
+    textShadow: "0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.7)",
+  },
   contactInfo: {
     display: "flex",
     flexDirection: "column",
     padding: "5vh 6vw",
     // justifyContent: "space-between",
+  },
+  aboutText: {
+    color: "black",
+    fontFamily: "Regular",
+    fontSize: "3vh",
+    padding: "3vh",
+    margin: "2vw",
+    textShadow: "0.125rem 0.125rem 0.25rem rgba(255, 255, 255, 0.8)",
+    backgroundColor: "#646464",
+    borderRadius: "1.25em",
   },
   aboutMe: {
     display: "flex",
