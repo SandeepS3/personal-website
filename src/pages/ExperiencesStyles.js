1da0f83d69f561914d64154571ff17974149cf5f
Sandeep Singh <78613787+SandeepS3@users.noreply.github.com>
2024-01-11 00:35:44 -0500

    Update ExperiencesStyles.js

--- src/pages/ExperiencesStyles.js
+++ src/pages/ExperiencesStyles.js
@@ -177,6 +177,7 @@ const mediaStyles = {
     backgroundColor: "#D9D9D9",
     borderRadius: "1.25em",
     width: "100%",
+    marginBottom:"1vh",
     boxShadow:
       "0 4px 4px #011B77, 0 4px 4px #011B77,inset 0 4px 4px #011B77, inset 0 0 35px black",
   },
